fdeca5ea0aa4282ccd78c9dbf3456024c05c1a7d
Ediberto Ponce <ediberto01@gmail.com>
2021-10-06 21:12:27 -0700

    Used the Format Document tool for our seed.js

--- models/seed.js
+++ models/seed.js
@@ -1,17 +1,15 @@
 const mongoose = require('mongoose')
 require('dotenv').config();
 
-async function seed () {
+async function seed() {
     mongoose.connect(process.env.DATABASE_URI);
-
     const Event = require('./eventschema.js');
-
     try {
         await Event.create({
             title: 'Wiggles Concert',
             descritpion: 'fun times',
             location: [],
-            formatted_address:'123 Sunshine Blvd, San Diego, CA 12345',
+            formatted_address: '123 Sunshine Blvd, San Diego, CA 12345',
             date: '12/12/21',
             email: 'blusteryday@gmail.com',
         });
